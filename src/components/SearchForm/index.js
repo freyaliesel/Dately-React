3c5e9e807971fdec6d4e5a34417d03a5c540d232
freyaliesel <80221739+freyaliesel@users.noreply.github.com>
2022-07-25 13:55:03 -0500

    added bare-bones search form to landing page

--- src/components/SearchForm/index.js
+++ src/components/SearchForm/index.js
@@ -1,53 +1,74 @@
-
 // Need to rework form to display
 
-const SearchForm = () => {
-    return (
-        <form autoComplete="off" className="white-text">
-        <div className="input-field">
-            {/* <input
-                className="datepicker validate white-text"
-                id="datepicker"
-                type="text"
-                required=""
-                aria-required="true"
-            />
-            <label> Date </label>
-            <span
-                className="helper-text"
-                data-error="Select a date"
-            ></span>
-        </div>
-        <div className="input-field">
-            <input
-                id="location"
-                className="autocomplete validate white-text"
-                required=""
-                aria-required="true"
-                type="text"
-            >
-                <label>Neighborhood</label>
-                <span
-                    className="helper-text"
-                    data-error="Enter a location"
-                    data-success=""
-                ></span>
-            </input> */}
-        </div>
-        <button
-            id="submit-btn"
-            className="xwaves-effect waves-light btn-small"
-        >
-            <i
-                id="search-icon"
-                className="medium material-icons"
-            >
-                search
-            </i>
-            Search
-        </button>
-    </form>
-    )
-}
+import { useState } from "react";
+import DatePicker from "react-datepicker"
 
-export default SearchForm
+import "react-datepicker/dist/react-datepicker.css"
+import "./SearchForm.css"
+
+const SearchForm = () => {
+    const [location, setLocation] = useState("");
+    const [date, setDate] = useState('');
+
+    const handleInputChange = (e) => {
+        const { value } = e.target;
+
+        setLocation(value)
+    };
+
+    const handleFormSubmit = (e) => {
+        e.preventDefault()
+        console.log(date, location)
+    }
+
+    return (
+        <>
+            <form autoComplete="off" className="white-text">
+                <div className="input-field white-text">
+                    <DatePicker 
+                    selected={date}
+                    onChange={(date:Date) => setDate(date)}
+                    placeholderText="Select a date"
+                    />
+                    {/* <label>Date</label> */}
+                    <span
+                        className="helper-text"
+                        dataerror="Select a date"
+                        datasuccess=""
+                    ></span>
+                </div>
+
+                <div className="input-field">
+                    <input
+                        className="autocomplete validate white-text"
+                        value={location}
+                        name="location"
+                        onChange={handleInputChange}
+                        type="text"
+                        placeholder="Select a location"
+                    />
+                    {/* <label>Location</label> */}
+                    <span
+                        className="helper-text"
+                        dataerror="Enter a location"
+                        datasuccess=""
+                    ></span>
+                </div>
+
+                <button
+                    type="button"
+                    id="submit-btn"
+                    className="xwaves-effect waves-light btn-small"
+                    onClick={handleFormSubmit}
+                >
+                    <i id="search-icon" className="medium material-icons">
+                        search
+                    </i>
+                    Search
+                </button>
+            </form>
+        </>
+    );
+};
+
+export default SearchForm;
